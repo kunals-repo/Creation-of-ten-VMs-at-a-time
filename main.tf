b096f8df782dd55e0b52edf86d181f7e04c61d7d
Kunal Yeole <kunalpatil9324@gmail.com>
2026-04-12 16:20:06 +0530

    vnet-rg created

--- main.tf
+++ main.tf
@@ -1,16 +1,7 @@
 
 resource "azurerm_resource_group" "rg-block" {
-  name = "storage-rg"
+  name = "vnet-rg"
   location = "eastus"
   
 }
 
-resource "azurerm_storage_account" "storage-block" {
-  name                     = "trunoteacct"
-  resource_group_name      = azurerm_resource_group.rg-block.name
-  location                 = azurerm_resource_group.rg-block.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-
-}
